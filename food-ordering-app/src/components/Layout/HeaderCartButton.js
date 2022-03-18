3ca3a89d01efd9c87a58d69453fee153904af811
unknown <dmello.shelton@yahoo.com>
2022-03-18 11:18:13 -0400

    Added context to manage cart data and cart functionality to add and remove items

--- food-ordering-app/src/components/Layout/HeaderCartButton.js
+++ food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,41 @@
-import React from "react";
-
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnisHiglighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numItems = items.reduce((curr, item) => {
+    return curr + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnisHiglighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {setBtnIsHighlighted(false)}, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numItems}</span>
     </button>
   );
 };
