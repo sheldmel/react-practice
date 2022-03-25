c3ddbcdd7ee2e365981f91d29628dbac15b7cfdd
unknown <dmello.shelton@yahoo.com>
2022-03-25 11:14:52 -0400

    Added actions to add, subtract and toggle visibility for counter

--- redux-practice/src/components/Counter.js
+++ redux-practice/src/components/Counter.js
@@ -1,12 +1,37 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const toggle = useSelector((state) => state.visibility);
+  const dispatch = useDispatch();
+  
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: "ADD" });
+  };
+
+  const decrementCounterHandler = () => {
+    dispatch({ type: "SUB" });
+  };
+
+  const incrementBy5CounterHandler = () => {
+    dispatch({ type: "ADD-BY", amount: 5 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{toggle ? counter : "HIDDEN"}</div>
+      <div>
+        <button onClick={incrementCounterHandler}>Increment</button>
+        <button onClick={incrementBy5CounterHandler}>Increment by 5</button>
+        <button onClick={decrementCounterHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- redux-practice/src/store/index.js
+++ redux-practice/src/store/index.js
@@ -1,15 +1,29 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, visibility: true }, action) => {
   switch (action.type) {
     case "ADD": {
       return {
         counter: state.counter + 1,
+        visibility: state.visibility
+      };
+    }
+    case "ADD-BY": {
+      return {
+        counter: state.counter + action.amount,
+        visibility: state.visibility
       };
     }
     case "SUB": {
       return {
         counter: state.counter - 1,
+        visibility: state.visibility
+      };
+    }
+    case "TOGGLE": {
+      return {
+        counter: state.counter,
+        visibility: !state.visibility
       };
     }
     default:
