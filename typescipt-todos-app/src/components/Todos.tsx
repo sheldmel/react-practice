5a4b3f6396d3a2960ede8a675ab477459c1f0d92
unknown <dmello.shelton@yahoo.com>
2022-03-30 16:01:32 -0400

    Added typescript todos app

---
+++ typescipt-todos-app/src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <ul>
+        <li>
+            <li>Learn React</li>
+            <li>Learn TypeScript</li>
+        </li>
+    </ul>
+  )
+}
+
+export default Todos
